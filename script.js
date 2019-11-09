8bc4218b870fd7f76e7de88182aab6fa9d4963bb
NalinKane <43729660+NalinKane@users.noreply.github.com>
2019-11-09 11:11:18 +0000

    added modals and tidied the script up (#13)
    * added modals and tidied the script up
    
    * removed backspace

--- script.js
+++ script.js
@@ -1,62 +1,101 @@
-
 const getLocationButton = document.querySelector(`#get-location`);
 const searchButton = document.querySelector(`#search-by-location`);
 const restaurantsReturned = document.querySelector(`#restaurant-returned`);
+const restaurantInfoModal = document.querySelector("#modal-restaurant-info");
+const modalToggle = document.querySelector("#open-modal");
+const modalClose = document.querySelector("#close-modal");
+
 let userLatitude = ``;
 let userLongitude = ``;
 
 getLocationButton.addEventListener(`click`, event => {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position){
-        userLatitude = position.coords.latitude;
-        userLongitude = position.coords.longitude;
-        fetch (`https://maps.googleapis.com/maps/api/geocode/json?latlng=${userLatitude},${userLongitude}&key=AIzaSyDUU-niRiyVVeS9DtXJswEOVHjiBJevfno`)
-            .then(response => response.json())
-            .then(data =>{
-                console.log(data)
-            });
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      userLatitude = position.coords.latitude;
+      userLongitude = position.coords.longitude;
+      fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${userLatitude},${userLongitude}&key=AIzaSyDUU-niRiyVVeS9DtXJswEOVHjiBJevfno`
+      )
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
         });
-        } else { 
-        getLocationButton.innerHTML = "N/A";
-    };
+    });
+  } else {
+    getLocationButton.innerHTML = "N/A";
+  }
+});
+
+function openModal(event) {
+  const targetID = event.target.getAttribute("data-id");
+  const restaurantCard = document.querySelector(`#${targetID}`);
+  console.log(restaurantCard);
+
+  const restaurantContent = restaurantCard.querySelector(
+    ".inside-restaurant-info"
+  );
+  const modalContent = document.querySelector("#modal-content");
+  modalContent.append(restaurantContent);
+
+  restaurantInfoModal.classList.add("modal-open");
+}
+
+function closeModal() {
+  console.log("modal closed");
+  restaurantInfoModal.classList.remove("modal-open");
+}
+
+document.addEventListener("click", function(event) {
+  if (!event.target.matches(".open-more-info")) return;
+  event.preventDefault();
+  openModal(event);
+});
+
+modalClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  closeModal();
 });
 
 searchButton.addEventListener(`click`, event => {
-    fetch(`https://developers.zomato.com/api/v2.1/search?lat=${userLatitude}&lon=${userLongitude}&apikey=969dccb114a560b6d4df35b25a8e6418`)
-        .then(repsonse => repsonse.json())
-        .then(data => {
-            console.log(data)
-            data.restaurants.forEach(restaurant =>{
-                const fragment = document.createDocumentFragment();
-                const restaurantCard = document.createElement(`div`);
-                const restaurantInfo = document.createElement(`div`);
-                const restaurantName = document.createElement(`h2`);
-                const restaurantCuisine = document.createElement(`p`);
-                const restaurantPhoto = document.createElement(`img`);
-                const averageCostForTwo = document.createElement(`p`);
-                const address = document.createElement(`p`);
-                const timings = document.createElement(`p`);
-                const phoneNumber = document.createElement(`p`);
-                const averageRating = document.createElement(`p`);
-                fragment.appendChild(restaurantCard);
-                restaurantCard.appendChild(restaurantName);
-                restaurantCard.appendChild(restaurantPhoto);
-                restaurantCard.appendChild(restaurantInfo);
-                restaurantCard.appendChild(phoneNumber);
-                restaurantCard.appendChild(averageRating);
-                restaurantInfo.appendChild(restaurantCuisine);
-                restaurantInfo.appendChild(averageCostForTwo);
-                restaurantInfo.appendChild(address);
-                restaurantInfo.appendChild(timings);
-                restaurantName.textContent = restaurant.restaurant.name;
-                restaurantPhoto.setAttribute(`src`, restaurant.restaurant.featured_image);
-                phoneNumber.textContent = `Call on : ${restaurant.restaurant.phone_numbers}`;
-                averageRating.textContent = `Avg.Rating ${restaurant.restaurant.user_rating.aggregate_rating}`;
-                restaurantCuisine.textContent = `Type: ${restaurant.restaurant.cuisines}`;
-                averageCostForTwo.textContent = `Avg. cost for two: ${restaurant.restaurant.average_cost_for_two}`;
-                address.textContent = restaurant.restaurant.location.address;
-                timings.textContent = restaurant.restaurant.timings;
-                restaurantsReturned.appendChild(fragment);
-            });
-        });
+  fetch(
+    `https://developers.zomato.com/api/v2.1/search?lat=${userLatitude}&lon=${userLongitude}&apikey=969dccb114a560b6d4df35b25a8e6418`
+  )
+    .then(repsonse => repsonse.json())
+    .then(({ restaurants }) => {
+      console.log(restaurants);
+      restaurants.map(({ restaurant }) => {
+        const {
+          name,
+          featured_image,
+          cuisines,
+          average_cost_for_two,
+          location,
+          timings,
+          phone_numbers,
+          user_rating,
+          id
+        } = restaurant;
+
+        const restaurantNode = `
+            <div class="restaurant-card" id="restaurant-${id}">
+                <img src="${featured_image}" class="photo" title="Photo of ${name}" />
+                <div class="content">
+                    <h2>${name}</h2>
+                    <p class="phone">Call on : ${phone_numbers}</p>
+                    <p class="rating">Avg.Rating ${user_rating.aggregate_rating}</p>
+                    <button class="open-more-info" data-id="restaurant-${id}">View more</button>
+                </div>
+                <div class="restaurant-info">
+                    <div class="inside-restaurant-info">
+                        <p class="couisine">Type: ${cuisines}</p>
+                        <p class="cost-for-two">Avg. cost for two: ${average_cost_for_two}</p>
+                        <p class="address">${location.address}</p>
+                        <p class="timings">${timings}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+        restaurantsReturned.innerHTML += restaurantNode;
+      });
+    });
 });
