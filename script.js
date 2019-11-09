4892f1326f9c3485e9686dd6d34bf1f86a158f9b
Neeraja Djearaman <neeru.jai@gmail.com>
2019-11-09 12:45:19 +0000

    map added

--- script.js
+++ script.js
@@ -1,101 +1,44 @@
-const getLocationButton = document.querySelector(`#get-location`);
-const searchButton = document.querySelector(`#search-by-location`);
-const restaurantsReturned = document.querySelector(`#restaurant-returned`);
-const restaurantInfoModal = document.querySelector("#modal-restaurant-info");
-const modalToggle = document.querySelector("#open-modal");
-const modalClose = document.querySelector("#close-modal");
+fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${userLatitude},${userLongitude}&key=AIzaSyBs0DXvW7g88kD3-OS7i4HXHzl_oPAP1LQ`
+    )
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
 
-let userLatitude = ``;
-let userLongitude = ``;
 
-getLocationButton.addEventListener(`click`, event => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      userLatitude = position.coords.latitude;
-      userLongitude = position.coords.longitude;
-      fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${userLatitude},${userLongitude}&key=AIzaSyDUU-niRiyVVeS9DtXJswEOVHjiBJevfno`
-      )
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
+function initMap() {
+    let location = new Object();
+    navigator.geolocation.getCurrentPosition(function(pos) {
+        location.lat = pos.coords.latitude;
+        location.long = pos.coords.longitude;
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: location.lat, lng: location.long },
+            zoom: 15
         });
+        getRestaurants(location);
+
     });
-  } else {
-    getLocationButton.innerHTML = "N/A";
-  }
-});
-
-function openModal(event) {
-  const targetID = event.target.getAttribute("data-id");
-  const restaurantCard = document.querySelector(`#${targetID}`);
-  console.log(restaurantCard);
-
-  const restaurantContent = restaurantCard.querySelector(
-    ".inside-restaurant-info"
-  );
-  const modalContent = document.querySelector("#modal-content");
-  modalContent.append(restaurantContent);
-
-  restaurantInfoModal.classList.add("modal-open");
 }
 
-function closeModal() {
-  console.log("modal closed");
-  restaurantInfoModal.classList.remove("modal-open");
+function getRestaurants(location) {
+    var pyrmont = new google.maps.LatLng(location.lat, location.long);
+    var request = {
+        location: pyrmont,
+        radius: '1500',
+        type: ['restaurant']
+    };
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
 }
 
-document.addEventListener("click", function(event) {
-  if (!event.target.matches(".open-more-info")) return;
-  event.preventDefault();
-  openModal(event);
-});
-
-modalClose.addEventListener("click", function(event) {
-  event.preventDefault();
-  closeModal();
-});
-
-searchButton.addEventListener(`click`, event => {
-  fetch(
-    `https://developers.zomato.com/api/v2.1/search?lat=${userLatitude}&lon=${userLongitude}&apikey=969dccb114a560b6d4df35b25a8e6418`
-  )
-    .then(repsonse => repsonse.json())
-    .then(({ restaurants }) => {
-      console.log(restaurants);
-      restaurants.map(({ restaurant }) => {
-        const {
-          name,
-          featured_image,
-          cuisines,
-          average_cost_for_two,
-          location,
-          timings,
-          phone_numbers,
-          user_rating,
-          id
-        } = restaurant;
-
-        const restaurantNode = `
-            <div class="restaurant-card" id="restaurant-${id}">
-                <img src="${featured_image}" class="photo" title="Photo of ${name}" />
-                <div class="content">
-                    <h2>${name}</h2>
-                    <p class="phone">Call on : ${phone_numbers}</p>
-                    <p class="rating">Avg.Rating ${user_rating.aggregate_rating}</p>
-                    <button class="open-more-info" data-id="restaurant-${id}">View more</button>
-                </div>
-                <div class="restaurant-info">
-                    <div class="inside-restaurant-info">
-                        <p class="couisine">Type: ${cuisines}</p>
-                        <p class="cost-for-two">Avg. cost for two: ${average_cost_for_two}</p>
-                        <p class="address">${location.address}</p>
-                        <p class="timings">${timings}</p>
-                    </div>
-                </div>
-            </div>
-        `;
-        restaurantsReturned.innerHTML += restaurantNode;
-      });
-    });
-});
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServicesStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var palce = results[i];
+            let price = createPrice(place.price_level);
+            let content = `${place.name} ${plca.vicinity} `
+            console.log(results)
+        }
+    }
+}
