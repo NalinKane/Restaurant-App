d6f9164dad10cb8b762567fe31e221d2cddd5877
Neeraja <55536350+NeerajaDjea@users.noreply.github.com>
2019-11-12 20:03:16 +0000

    map added (#15)
    * map added
    
    * identified latitude and longitude
    
    * map and marker added

--- script.js
+++ script.js
@@ -9,81 +9,79 @@ let userLatitude = ``;
 let userLongitude = ``;
 
 getLocationButton.addEventListener(`click`, event => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      userLatitude = position.coords.latitude;
-      userLongitude = position.coords.longitude;
-      fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${userLatitude},${userLongitude}&key=AIzaSyDUU-niRiyVVeS9DtXJswEOVHjiBJevfno`
-      )
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            userLatitude = position.coords.latitude;
+            userLongitude = position.coords.longitude;
+            fetch(
+                    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${userLatitude},${userLongitude}&key=AIzaSyDUU-niRiyVVeS9DtXJswEOVHjiBJevfno`
+                )
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                });
         });
-    });
-  } else {
-    getLocationButton.innerHTML = "N/A";
-  }
+    } else {
+        getLocationButton.innerHTML = "N/A";
+    }
 });
 
 function openModal(event) {
-  const targetID = event.target.getAttribute("data-id");
-  const restaurantCard = document.querySelector(`#${targetID}`);
-  console.log(restaurantCard);
+    const targetID = event.target.getAttribute("data-id");
+    const restaurantCard = document.querySelector(`#${targetID}`);
+    console.log(restaurantCard);
 
-  const restaurantContent = restaurantCard.querySelector(
-    ".inside-restaurant-info"
 
-  ).cloneNode(true);
-  const modalContent = document.querySelector("#modal-content");
+    const restaurantContent = restaurantCard.querySelector(
+        ".inside-restaurant-info"
+
+    ).cloneNode(true);
+    const modalContent = document.querySelector("#modal-content");
     modalContent.append(restaurantContent);
 
-
-  restaurantInfoModal.classList.add("modal-open");
+    restaurantInfoModal.classList.add("modal-open");
 }
 
 function closeModal() {
 
-  
-  restaurantInfoModal.classList.remove("modal-open");
-  const modalContent = document.querySelector("#modal-content");
-  modalContent.innerHTML = "";
+    restaurantInfoModal.classList.remove("modal-open");
+    const modalContent = document.querySelector("#modal-content");
+    modalContent.innerHTML = "";
 
 }
 
 document.addEventListener("click", function(event) {
-  if (!event.target.matches(".open-more-info")) return;
-  event.preventDefault();
-  openModal(event);
+    if (!event.target.matches(".open-more-info")) return;
+    event.preventDefault();
+    openModal(event);
 });
 
 modalClose.addEventListener("click", function(event) {
-  event.preventDefault();
-  closeModal();
-
+    event.preventDefault();
+    closeModal();
 });
 
 searchButton.addEventListener(`click`, event => {
-  fetch(
-    `https://developers.zomato.com/api/v2.1/search?lat=${userLatitude}&lon=${userLongitude}&apikey=969dccb114a560b6d4df35b25a8e6418`
-  )
-    .then(repsonse => repsonse.json())
-    .then(({ restaurants }) => {
-      console.log(restaurants);
-      restaurants.map(({ restaurant }) => {
-        const {
-          name,
-          featured_image,
-          cuisines,
-          average_cost_for_two,
-          location,
-          timings,
-          phone_numbers,
-          user_rating,
-          id
-        } = restaurant;
+    fetch(
+            `https://developers.zomato.com/api/v2.1/search?lat=${userLatitude}&lon=${userLongitude}&apikey=969dccb114a560b6d4df35b25a8e6418`
+        )
+        .then(repsonse => repsonse.json())
+        .then(({ restaurants }) => {
+            console.log(restaurants);
+            restaurants.map(({ restaurant }) => {
+                const {
+                    name,
+                    featured_image,
+                    cuisines,
+                    average_cost_for_two,
+                    location,
+                    timings,
+                    phone_numbers,
+                    user_rating,
+                    id
+                } = restaurant;
 
-        const restaurantNode = `
+                const restaurantNode = `
             <div class="restaurant-card" id="restaurant-${id}">
                 <img src="${featured_image}" class="photo" title="Photo of ${name}" />
                 <div class="content">
@@ -101,7 +99,9 @@ searchButton.addEventListener(`click`, event => {
                 </div>
             </div>
         `;
-        restaurantsReturned.innerHTML += restaurantNode;
-      });
-    });
+
+                restaurantsReturned.innerHTML += restaurantNode;
+            });
+        });
+
 });
